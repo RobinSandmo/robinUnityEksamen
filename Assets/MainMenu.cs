ea551a91f29e8b7b729474ac6905b237ea6eb4bf
Robin <robinsandmo@live.com>
2020-04-16 13:24:35 +0200

    master updatet

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    public Canvas menu;
+     bool MainMenuIsOpen;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MainMenuIsOpen = !MainMenuIsOpen;
+
+            if (MainMenuIsOpen)
+            {
+                menu.enabled = true;
+                Time.timeScale = 0;
+            }
+            if (!MainMenuIsOpen)
+            {
+                menu.enabled = false;
+                Time.timeScale = 1;
+            }
+        }
+    }   
+
+    public void PlayGame()
+	{
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+    public void QuitMenu() 
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
